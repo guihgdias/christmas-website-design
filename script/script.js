a3de65824dc8699087d065c99fe69a4b9c8b9a39
Guilherme Henrique <guilhermehgdiasbr@gmail.com>
2021-12-24 18:08:07 -0300

    Added Home

--- script/script.js
+++ script/script.js
@@ -23,4 +23,17 @@ window.onscroll = () => {
   navBar.classList.remove('active');
   searchBtn.classList.remove('fa-times');
   searchForm.classList.remove('active');
+
+  if(window.scrollY > 600) {
+    document.querySelector('.header').classList.add('active');
+  } else {
+    document.querySelector('.header').classList.remove('active');
+  }
+
+  let value = window.scrollY / 10;
+  document.querySelector('.home .mountain').style.bottom = `${-value}%`
+  document.querySelector('.home .content').style.bottom = `${value}%`
+  document.querySelector('.home .moon').style.marginTop = `${-value}%`
+  document.querySelector('.home .cloud-1').style.left = `${-value}%`
+  document.querySelector('.home .cloud-2').style.right = `${-value}%`
 };
